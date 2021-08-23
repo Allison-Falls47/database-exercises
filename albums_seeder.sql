6d85a1bb0d51e2d3aaa44202c7f50be997133a99
Allison Falls <allison.falls2@gmail.com>
2021-08-23 15:15:20 -0400

    finished seeder script

---
+++ albums_seeder.sql
@@ -0,0 +1,12 @@
+use codeup_test_db;
+
+insert into albums(artist, name, release_date, sales, genre)
+values ('Jackson','thriller','1982',47.9,'pop'),
+       ('AC/DC','Back in Black','1980',29.6,'Hard rock'),
+        ('Meat Loaf','Bat Out of Hell','1977',21.7,'hard rock'),
+    ('Pink Floyd','Dark Side of the Moon','1973',24.4,'progressive rock'),
+    ('Shania Twain','Come On Over','1997',29.6,'country');
+
+SELECT * from albums;
+
+
