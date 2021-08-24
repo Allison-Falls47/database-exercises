a149b93aaaaa9a9381dc0e0e97cd7b11ec8a8268
Allison Falls <allison.falls2@gmail.com>
2021-08-24 12:46:56 -0400

    fixed part two

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,10 +5,22 @@ SELECT  first_name
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya','Maya');
 
+SELECT  first_name
+FROM employees
+WHERE first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
+
 SELECT first_name
 FROM employees
 WHERE first_name like 'E%';
 
+SELECT first_name
+FROM employees
+WHERE first_name like 'E%' or last_name like '%e';
+
 SELECT last_name
 FROM employees
-WHERE last_name like 'q%';
+WHERE last_name like '%q%';
+
+SELECT last_name
+FROM employees
+WHERE last_name like '%q%' and last_name not like '%qu%';
