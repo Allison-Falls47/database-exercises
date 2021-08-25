c9449528a96a21cd0aa4aaa75b23fb3ac00fc021
Allison Falls <allison.falls2@gmail.com>
2021-08-25 10:27:21 -0400

    finished excercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -10,8 +10,10 @@ WHERE year(birth_date) BETWEEN 1950 AND 2020
   AND month(birth_date) = 12
   AND day(birth_date) = 25;
 
-SELECT *
+SELECT DATEDIFF(now(), hire_date)
 FROM employees
 WHERE year(hire_date) BETWEEN 1990 AND 1999
 AND month(birth_date) = 12
-  AND day(birth_date) = 25;
+  AND day(birth_date) = 25
+ORDER BY hire_date desc;
+
