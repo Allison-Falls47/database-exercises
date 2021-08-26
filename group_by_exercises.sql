87d33c6c6bfaf8f8730ab6201b3d793f868dae7f
Allison Falls <allison.falls2@gmail.com>
2021-08-26 12:46:52 -0400

    fixed gender

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -3,3 +3,15 @@ use employees;
 SELECT DISTINCT title
 FROM employees.titles;
 
+SELECT distinct first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%E'
+GROUP BY first_name, last_name
+order by last_name;
+
+
+select  gender, COUNT(gender)
+from employees
+where first_name in ('Irena', 'Vidya','Maya')
+group by gender;
+
