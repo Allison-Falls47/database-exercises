23e747f703f821ff058f73640316390317b8f975
Allison Falls <allison.falls2@gmail.com>
2021-08-30 12:19:28 -0400

    finished plus bonus

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,9 +1,42 @@
 use employees;
 
-SELECT CONCAT(dept_name, ' ', e.emp_no) AS full_name, d.dept_name
-FROM employees as e
-         JOIN dept_emp as de
-              ON de.emp_no = e.emp_no
-         JOIN departments as d
-              ON d.dept_no = de.dept_no
-WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
+select d.dept_name as 'Department Name'
+, concat(e.first_name, ' ', e.last_name) as 'Department Manager'
+from departments d
+         inner join dept_manager dm on d.dept_no = dm.dept_no
+         inner join employees e on dm.emp_no = e.emp_no
+where dm.to_date = '9999-01-01';
+
+select d.dept_name as 'Department Name'
+     , concat(e.first_name, ' ', e.last_name) as 'Department Manager'
+from departments d
+         inner join dept_manager dm on d.dept_no = dm.dept_no
+         inner join employees e on dm.emp_no = e.emp_no
+where dm.to_date = '9999-01-01' and e.gender = 'F';
+
+select t.title, count(t.emp_no)
+from departments d
+    inner join dept_emp de on d.dept_no = de.dept_no
+    inner join titles t on de.emp_no = t.emp_no
+where d.dept_name = 'Customer Service' and de.to_date = '9999-01-01'
+and t.to_date ='9999-01-01'
+group by t.title;
+
+select d.dept_name as 'Department Name'
+     , concat(e.first_name, ' ', e.last_name) as 'Department Manager'
+     , s.salary as 'Salary'
+from departments d
+         inner join dept_manager dm on d.dept_no = dm.dept_no
+         inner join employees e on dm.emp_no = e.emp_no
+         inner join salaries s on e.emp_no = s.emp_no
+where dm.to_date = '9999-01-01' and s.to_date = '9999-01-01';
+
+select concat(e.first_name, ' ',e.last_name) as Employee
+, d.dept_name as Department
+, concat(edm.first_name, ' ',edm.last_name) as Manager
+from departments as d
+        inner join dept_emp de on d.dept_no = de.dept_no
+        inner join employees e on de.emp_no = e.emp_no
+        inner join dept_manager dm on d.dept_no = dm.dept_no
+        inner join employees as edm on dm.emp_no = edm.emp_no
+where de.to_date - '9999-01-01' and dm.to_date = '9999-01-01'
