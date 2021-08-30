5e419d6c15306588d45b0eafe27043a2f60302d2
Allison Falls <allison.falls2@gmail.com>
2021-08-30 16:29:15 -0400

    finishd excercise

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,8 +1,26 @@
 use employees;
 
-SELECT first_name, last_name, hire_date
-FROM employees
-WHERE emp_no IN (
-    SELECT emp_no = '101010'
-    FROM dept_manager
+SELECT *
+FROM employees.employees no
+WHERE no.hire_date IN (
+    SELECT e.hire_date
+    FROM  employees.employees e
+    where e.emp_no = 101010
 );
+
+select distinct t.title
+from titles t
+where t.emp_no in (
+    select emp.emp_no from employees.employees emp
+    where emp.first_name = 'Aamod'
+    );
+
+select emp.first_name, emp.last_name
+from employees.employees emp
+where emp.gender = 'F' and (
+    emp.emp_no in (
+        select dm.emp_no
+        from dept_manager dm
+        where current_date between dm.from_date and dm.to_date
+        )
+    );
